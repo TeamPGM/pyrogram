08f07d4bbb9f7ffa7086078eea304f82c7cbda7d
xtaodada <xtao@xtao.de>
2025-08-26 22:24:22 +0800

    fix restarting of session on OsError

--- pyrogram/session/session.py
+++ pyrogram/session/session.py
@@ -20,6 +20,7 @@ from __future__ import annotations as _annotations
 
 import asyncio
 import bisect
+import contextlib
 import logging
 import os
 from enum import Enum, auto
@@ -304,14 +305,16 @@ class Session:
         self.ping_task_event.set()
 
         if self.ping_task is not None:
-            await self.ping_task
+            with contextlib.suppress(Exception):
+                await self.ping_task
 
         self.ping_task_event.clear()
 
         await self.connection.close()
 
         if self.recv_task:
-            await self.recv_task
+            with contextlib.suppress(Exception):
+                await self.recv_task
             self.recv_task = None
 
         await self._wait_pending_tasks()
